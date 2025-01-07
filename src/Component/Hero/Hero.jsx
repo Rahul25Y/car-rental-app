107dab573224790b489ae2781b162a0361662cb5
Rahul25Y <ry674661@gmail.com>
2025-01-07 21:45:53 +0530

    feat: dot

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -38,7 +38,7 @@ export default function Hero({ theme }) {
             <p data-aos="fade-up" data-aos-delay="1000">
               {" "}
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-              veritatis explicabo quibusdam quae reprehenderit ab{" "}
+              veritatis explicabo quibusdam quae reprehenderit ab.{" "}
             </p>
             <button data-aos="fade-up"
               data-aos-delay="1500"
